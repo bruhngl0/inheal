144ab75400f326e58217b2dc6da4a24c3ac7e037
bruhngl0 <adityashrm500@gmail.com>
2026-01-05 11:35:59 +0530

    config-bug

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,17 +5,15 @@ const Hero = () => {
     <div className="hero">
       {/* Mobile Video */}
       <video className="hero-main hero-mobile" autoPlay loop muted playsInline>
-        <source src="/vid-mob.mp4" type="video/mp4" />
+        <source src="/newmo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
+
       {/* PC/Desktop Video */}
       <video className="hero-main hero-desktop" autoPlay loop muted playsInline>
         <source src="/inpc.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
- 
     </div>
   );
 };
